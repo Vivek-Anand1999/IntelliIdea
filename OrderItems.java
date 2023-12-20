863efe004b828e3508f8fe8e93cbb31690fc4656
Vivek-Anand1999 <vivekanand8867@gmail.com>
2023-12-21 04:41:21 +0530

    adding new feature add to cart

--- OrderItems.java
+++ OrderItems.java
@@ -1,7 +1,10 @@
 import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,15 +12,20 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class OrderItems {
     public static void addingItemsAtCart(WebDriver driver) {
-        String[] inputData = {"Carrot", "Tomato", "Cucumber", "Broccoli", "Cauliflower"};
+        String[] inputData = {"Carrot", "Tomato", "Cucumber", "Brocolli", "Cauliflower"}; // Array save memory as compared to array list(we can convert array but during the run time will convert into array list
         List<WebElement> productName = driver.findElements(By.cssSelector("h4.product-name"));
-        for (String inputDatum : inputData) {
-            for (int i = 0; i < productName.size(); i++) {
-                String options = productName.get(i).getText();
-                if (options.contains(inputDatum)) {
-                    // ! need to add cart
-                    System.out.println("found");
-                    driver.findElements(By.xpath("//button[text()='ADD TO CART']")).get(i).click();
+        int count = 0;
+        for (int i = 0; i < productName.size(); i++) {
+            String[] options = productName.get(i).getText().split("-");
+            String purItemName = options[0].trim();
+            System.out.println(purItemName);
+            List<String> itemName = Arrays.asList(inputData);
+//            List<String> itemsNeededList= Collections.singletonList(pureData[0]); //! If you need a list with a single element, and you want the list to be immutable (not allow modifications),
+//            List<String> itemsNeededList= Arrays.asList(pureData[0]); //!If you need a more flexible list that allows modifications and reflects changes to the original array, you can use Arrays.asList.
+            if (itemName.contains(purItemName)) {
+                count++;
+                driver.findElements(By.xpath("//button[text()='ADD TO CART']")).get(i).click();
+                if (count == 5) {
                     break;
                 }
             }
@@ -30,6 +38,7 @@ public class OrderItems {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
-
+        //! calling methods:-
+        addingItemsAtCart(driver);
     }
 }
